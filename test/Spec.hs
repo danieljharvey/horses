b949c15c5aa77cec7892c842e9cd12daed17ebe5
Daniel Harvey <danieljamesharvey@gmail.com>
2020-06-16 13:11:01 +0100

    Fix tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,6 +7,7 @@ module Main where
 import Data.Text (Text)
 import qualified Data.Text as T
 import Language.Mimsa
+import Test.Helpers
 import Test.Hspec
 import qualified Test.Interpreter as Interpreter
 import Test.QuickCheck.Instances ()
@@ -19,28 +20,28 @@ charListToText = foldr T.cons ""
 
 exprs :: [(Expr, Either Text MonoType)]
 exprs =
-  [ (MyInt 1, Right MTInt),
-    (MyBool True, Right MTBool),
-    ( MyString
+  [ (int 1, Right MTInt),
+    (bool True, Right MTBool),
+    ( str
         (StringType "hello"),
       Right MTString
     ),
     (MyVar (mkName "x"), Left "Unknown variable \"x\""),
-    (MyLet (mkName "x") (MyInt 42) (MyBool True), Right MTBool),
-    (MyLet (mkName "x") (MyInt 42) (MyVar (mkName "x")), Right MTInt),
+    (MyLet (mkName "x") (int 42) (bool True), Right MTBool),
+    (MyLet (mkName "x") (int 42) (MyVar (mkName "x")), Right MTInt),
     ( MyLet
         (mkName "x")
-        (MyBool True)
-        (MyLet (mkName "y") (MyInt 42) (MyVar (mkName "x"))),
+        (bool True)
+        (MyLet (mkName "y") (int 42) (MyVar (mkName "x"))),
       Right MTBool
     ),
     ( MyLet
         (mkName "x")
-        (MyBool True)
-        (MyLet (mkName "x") (MyInt 42) (MyVar (mkName "x"))),
+        (bool True)
+        (MyLet (mkName "x") (int 42) (MyVar (mkName "x"))),
       Right MTInt
     ),
-    ( MyLambda (mkName "x") (MyBool True),
+    ( MyLambda (mkName "x") (bool True),
       Right $ MTFunction (MTUnknown (UniVar 1)) MTBool
     ),
     ( identity,
@@ -55,23 +56,23 @@ exprs =
     ( MyApp
         ( MyLambda
             (mkName "x")
-            (MyBool True)
+            (bool True)
         )
-        (MyInt 1),
+        (int 1),
       Right MTBool
     ),
     ( MyApp
         identity
-        (MyInt 1),
+        (int 1),
       Right MTInt
     ),
     ( MyApp
         ( MyLambda
             (mkName "x")
-            ( (MyIf (MyVar (mkName "x")) (MyInt 10) (MyInt 10))
+            ( (MyIf (MyVar (mkName "x")) (int 10) (int 10))
             )
         )
-        (MyInt 100),
+        (int 100),
       Left "Can't match MTBool with MTInt"
     ),
     ( MyLambda (mkName "x") (MyApp (MyVar (mkName "x")) (MyVar (mkName "x"))),
@@ -93,8 +94,8 @@ main = hspec $ do
       _ <- traverse (\(code, expected) -> startInference code `shouldBe` expected) exprs
       pure ()
     it "We can use identity with two different datatypes in one expression" $ do
-      let lambda = (MyLambda (mkName "x") (MyIf (MyApp identity (MyVar (mkName "x"))) (MyApp identity (MyInt 1)) (MyApp identity (MyInt 2))))
-      let expr = MyApp lambda (MyBool True)
+      let lambda = (MyLambda (mkName "x") (MyIf (MyApp identity (MyVar (mkName "x"))) (MyApp identity (int 1)) (MyApp identity (int 2))))
+      let expr = MyApp lambda (bool True)
       (startInference lambda) `shouldBe` Right (MTFunction (MTUnknown 1) MTInt)
       (startInference expr) `shouldBe` Right MTInt
 {-  describe "Serialisation" $ do
