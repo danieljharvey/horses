d0728acc11704bdee80f226e289c6fb7885ea62b
Daniel Harvey <danieljamesharvey@gmail.com>
2020-07-06 18:22:47 +0100

    Newtype for Substitutions (#16)

--- src/Language/Mimsa/Typechecker/Infer.hs
+++ src/Language/Mimsa/Typechecker/Infer.hs
@@ -49,37 +49,18 @@ inferBuiltIn name = case getLibraryFunction name of
 instantiate :: Scheme -> App MonoType
 instantiate (Scheme vars ty) = do
   newVars <- traverse (const getUnknown) vars
-  let subst = M.fromList (zip vars newVars)
+  let subst = Substitutions $ M.fromList (zip vars newVars)
   pure (applySubst subst ty)
 
 applySubstScheme :: Substitutions -> Scheme -> Scheme
-applySubstScheme subst (Scheme vars t) =
+applySubstScheme (Substitutions subst) (Scheme vars t) =
   -- The fold takes care of name shadowing
-  Scheme vars (applySubst (foldr M.delete subst vars) t)
+  Scheme vars (applySubst subs t)
+  where
+    subs = Substitutions $ foldr M.delete subst vars
 
 applySubstCtx :: Substitutions -> Environment -> Environment
 applySubstCtx subst ctx = M.map (applySubstScheme subst) ctx
-
-applySubst :: Substitutions -> MonoType -> MonoType
-applySubst subst ty = case ty of
-  MTVar i ->
-    fromMaybe (MTVar i) (M.lookup i subst)
-  MTFunction arg res ->
-    MTFunction (applySubst subst arg) (applySubst subst res)
-  MTPair a b ->
-    MTPair
-      (applySubst subst a)
-      (applySubst subst b)
-  MTList a -> MTList (applySubst subst a)
-  MTRecord a -> MTRecord (applySubst subst <$> a)
-  MTSum a b -> MTSum (applySubst subst a) (applySubst subst b)
-  MTInt -> MTInt
-  MTString -> MTString
-  MTBool -> MTBool
-  MTUnit -> MTUnit
-
-composeSubst :: Substitutions -> Substitutions -> Substitutions
-composeSubst s1 s2 = M.union (M.map (applySubst s1) s2) s1
 
 findSwappedName :: Name -> App SwappedName
 findSwappedName name = SwappedName <$> fromMaybe name <$> asks (M.lookup name)
@@ -102,7 +83,7 @@ inferFuncReturn env binder function tyArg = do
   (s1, tyFun) <- infer newEnv function
   s2 <- unify (MTFunction tyArg tyFun) (applySubst s1 tyRes)
   let s3 = mempty
-      subs = s3 `composeSubst` s2 `composeSubst` s1
+      subs = s3 <> s2 <> s1
   pure (subs, applySubst subs tyFun)
 
 --traceAnd :: (Show a) => Text -> a -> a
@@ -115,7 +96,7 @@ inferList env (a :| as) = do
         (s', ty') <- as'
         (sA, tyB) <- infer env a'
         sB <- unify ty' tyB
-        pure (sB `composeSubst` sA `composeSubst` s', applySubst sB tyB)
+        pure (sB <> sA <> s', applySubst sB tyB)
   foldl
     foldFn
     (pure (s1, tyA))
@@ -128,7 +109,7 @@ splitRecordTypes map' = (subs, MTRecord types)
   where
     subs =
       foldr
-        composeSubst
+        (<>)
         mempty
         ((fst . snd) <$> M.toList map')
     types = snd <$> map'
@@ -146,15 +127,15 @@ infer env (MyRecord map') = do
   (s1, tyResult) <- splitRecordTypes <$> traverse (infer env) map'
   s2 <- unify tyResult tyRecord
   pure
-    ( s2 `composeSubst` s1,
-      applySubst (s2 `composeSubst` s1) tyRecord
+    ( s2 <> s1,
+      applySubst (s2 <> s1) tyRecord
     )
 infer env (MyLet binder expr body) = do
   (s1, tyExpr) <- infer env expr
   let scheme = Scheme [] (applySubst s1 tyExpr)
   let newEnv = M.insert binder scheme env
   (s2, tyBody) <- infer (applySubstCtx s1 newEnv) body
-  pure (s2 `composeSubst` s1, tyBody)
+  pure (s2 <> s1, tyBody)
 infer env (MyRecordAccess (MyRecord items') name) = do
   case M.lookup name items' of
     Just item -> do
@@ -177,7 +158,7 @@ infer env (MyRecordAccess a name) = do
     unify
       (MTRecord $ M.singleton name tyResult)
       tyItems
-  let subs = s2 `composeSubst` s1
+  let subs = s2 <> s1
   pure (subs, applySubst subs tyResult)
 infer env (MyCase sumExpr (MyLambda binderL exprL) (MyLambda binderR exprR)) = do
   (s1, tySum) <- infer env sumExpr
@@ -189,14 +170,14 @@ infer env (MyCase sumExpr (MyLambda binderL exprL) (MyLambda binderR exprR)) = d
       pure (tyL, tyR)
     otherType -> throwError $ CaseMatchExpectedSum otherType
   s2 <- unify (MTSum tyL tyR) tySum
-  (s3, tyLeftRes) <- inferFuncReturn env binderL exprL (applySubst (s2 `composeSubst` s1) tyL)
-  (s4, tyRightRes) <- inferFuncReturn env binderR exprR (applySubst (s2 `composeSubst` s1) tyR)
+  (s3, tyLeftRes) <- inferFuncReturn env binderL exprL (applySubst (s2 <> s1) tyL)
+  (s4, tyRightRes) <- inferFuncReturn env binderR exprR (applySubst (s2 <> s1) tyR)
   let subs =
-        s4 `composeSubst` s3
-          `composeSubst` s2
-          `composeSubst` s1
+        s4 <> s3
+          <> s2
+          <> s1
   s5 <- unify tyLeftRes tyRightRes
-  pure (s5 `composeSubst` subs, applySubst (s5 `composeSubst` subs) tyLeftRes)
+  pure (s5 <> subs, applySubst (s5 <> subs) tyLeftRes)
 infer _env (MyCase _ l r) = throwError $ CaseMatchExpectedLambda l r
 infer env (MyLetPair binder1 binder2 expr body) = do
   (s1, tyExpr) <- infer env expr
@@ -211,8 +192,8 @@ infer env (MyLetPair binder1 binder2 expr body) = do
       schemeB = Scheme [] (applySubst s1 tyB)
       newEnv = M.insert binder1 schemeA (M.insert binder2 schemeB env)
   s2 <- unify tyExpr (MTPair tyA tyB)
-  (s3, tyBody) <- infer (applySubstCtx (s2 `composeSubst` s1) newEnv) body
-  pure (s3 `composeSubst` s2 `composeSubst` s1, tyBody)
+  (s3, tyBody) <- infer (applySubstCtx (s2 <> s1) newEnv) body
+  pure (s3 <> s2 <> s1, tyBody)
 infer env (MyLetList binder1 binder2 expr body) = do
   (s1, tyExpr) <- infer env expr
   (tyHead, tyRest) <- case tyExpr of
@@ -229,8 +210,8 @@ infer env (MyLetList binder1 binder2 expr body) = do
       newEnv = M.insert binder1 schemeHead (M.insert binder2 schemeRest env)
   s2 <- unify tyExpr (MTList tyHead)
   s3 <- unify tyRest (MTSum MTUnit (MTList tyHead))
-  (s4, tyBody) <- infer (applySubstCtx (s3 `composeSubst` s2 `composeSubst` s1) newEnv) body
-  pure (s4 `composeSubst` s3 `composeSubst` s2 `composeSubst` s1, tyBody)
+  (s4, tyBody) <- infer (applySubstCtx (s3 <> s2 <> s1) newEnv) body
+  pure (s4 <> s3 <> s2 <> s1, tyBody)
 infer env (MyLambda binder body) = do
   tyBinder <- getUnknown
   let tmpCtx = M.insert binder (Scheme [] tyBinder) env
@@ -241,17 +222,17 @@ infer env (MyApp function argument) = do
   (s1, tyFun) <- infer env function
   (s2, tyArg) <- infer (applySubstCtx s1 env) argument
   s3 <- unify (applySubst s2 tyFun) (MTFunction tyArg tyRes)
-  pure (s3 `composeSubst` s2 `composeSubst` s1, applySubst s3 tyRes)
+  pure (s3 <> s2 <> s1, applySubst s3 tyRes)
 infer env (MyIf condition thenCase elseCase) = do
   (s1, tyCond) <- infer env condition
   (s2, tyThen) <- infer (applySubstCtx s1 env) thenCase
-  (s3, tyElse) <- infer (applySubstCtx (s2 `composeSubst` s1) env) elseCase
+  (s3, tyElse) <- infer (applySubstCtx (s2 <> s1) env) elseCase
   s4 <- unify tyThen tyElse
   s5 <- unify tyCond MTBool
   let subs =
-        s5 `composeSubst` s4 `composeSubst` s3
-          `composeSubst` s2
-          `composeSubst` s1
+        s5 <> s4 <> s3
+          <> s2
+          <> s1
   pure
     ( subs,
       applySubst subs tyElse
@@ -259,7 +240,7 @@ infer env (MyIf condition thenCase elseCase) = do
 infer env (MyPair a b) = do
   (s1, tyA) <- infer env a
   (s2, tyB) <- infer (applySubstCtx s1 env) b
-  pure (s2 `composeSubst` s1, MTPair tyA tyB)
+  pure (s2 <> s1, MTPair tyA tyB)
 infer env (MySum MyLeft left') = do
   tyRight <- getUnknown
   (s1, tyLeft) <- infer env left'
@@ -286,22 +267,22 @@ varBind var ty
     actualName <- findSwappedName var
     throwError $
       FailsOccursCheck actualName
-  | otherwise = pure (M.singleton var ty)
+  | otherwise = pure $ Substitutions (M.singleton var ty)
 
 unify :: MonoType -> MonoType -> App Substitutions
 unify a b | a == b = pure mempty
 unify (MTFunction l r) (MTFunction l' r') = do
   s1 <- unify l l'
   s2 <- unify (applySubst s1 r) (applySubst s1 r')
-  pure (s2 `composeSubst` s1)
+  pure (s2 <> s1)
 unify (MTPair a b) (MTPair a' b') = do
   s1 <- unify a a'
   s2 <- unify (applySubst s1 b) (applySubst s1 b')
-  pure (s2 `composeSubst` s1)
+  pure (s2 <> s1)
 unify (MTSum a b) (MTSum a' b') = do
   s1 <- unify a a'
   s2 <- unify (applySubst s1 b) (applySubst s1 b')
-  pure (s2 `composeSubst` s1)
+  pure (s2 <> s1)
 unify (MTVar u) t = varBind u t
 unify (MTList a) (MTList a') = unify a a'
 unify (MTRecord as) (MTRecord bs) = do
@@ -311,7 +292,7 @@ unify (MTRecord as) (MTRecord bs) = do
         tyRight <- getTypeOrFresh k bs
         unify tyLeft tyRight
   s <- traverse getRecordTypes allKeys
-  pure (foldl composeSubst mempty s)
+  pure (foldl (<>) mempty s)
 unify t (MTVar u) = varBind u t
 unify a b =
   throwError $ UnificationError a b
--- src/Language/Mimsa/Types/Typechecker.hs
+++ src/Language/Mimsa/Types/Typechecker.hs
@@ -3,10 +3,37 @@
 module Language.Mimsa.Types.Typechecker where
 
 import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
 import Language.Mimsa.Types.MonoType
 import Language.Mimsa.Types.Name
 import Language.Mimsa.Types.Scheme
 
 type Environment = Map Name Scheme
 
-type Substitutions = Map Name MonoType
+newtype Substitutions = Substitutions {getSubstitutions :: Map Name MonoType}
+
+instance Semigroup Substitutions where
+  (Substitutions s1) <> (Substitutions s2) =
+    Substitutions $ M.union (M.map (applySubst (Substitutions s1)) s2) s1
+
+instance Monoid Substitutions where
+  mempty = Substitutions mempty
+
+applySubst :: Substitutions -> MonoType -> MonoType
+applySubst subst ty = case ty of
+  MTVar i ->
+    fromMaybe (MTVar i) (M.lookup i (getSubstitutions subst))
+  MTFunction arg res ->
+    MTFunction (applySubst subst arg) (applySubst subst res)
+  MTPair a b ->
+    MTPair
+      (applySubst subst a)
+      (applySubst subst b)
+  MTList a -> MTList (applySubst subst a)
+  MTRecord a -> MTRecord (applySubst subst <$> a)
+  MTSum a b -> MTSum (applySubst subst a) (applySubst subst b)
+  MTInt -> MTInt
+  MTString -> MTString
+  MTBool -> MTBool
+  MTUnit -> MTUnit
