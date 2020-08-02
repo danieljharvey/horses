ebf55ce2061966ed11b972ce55b42106446fb383
Daniel Harvey <danieljamesharvey@gmail.com>
2020-08-02 21:36:03 +0100

    Clean repl

--- src/Language/Mimsa/Repl/Actions.hs
+++ src/Language/Mimsa/Repl/Actions.hs
@@ -25,6 +25,32 @@ import Language.Mimsa.Types
 
 doReplAction :: Project -> ReplAction -> IO Project
 doReplAction env Help = do
+  doHelp
+  pure env
+doReplAction env (ListBindings) = do
+  doListBindings env
+  pure env
+doReplAction env Tui = do
+  goTui env
+doReplAction env (Versions name) = do
+  doVersions env name
+  pure env
+doReplAction env Watch = do
+  doWatch env
+  pure env
+doReplAction env (Evaluate expr) = doEvaluate env expr
+doReplAction env (Tree expr) = do
+  doTree env expr
+  pure env
+doReplAction env (Info expr) = do
+  doInfo env expr
+  pure env
+doReplAction env (Bind name expr) = doBind env name expr
+
+----------
+
+doHelp :: IO ()
+doHelp = do
   T.putStrLn "~~~ MIMSA ~~~"
   T.putStrLn ":help - this help screen"
   T.putStrLn ":info <expr> - get the type of <expr>"
@@ -34,17 +60,50 @@ doReplAction env Help = do
   T.putStrLn ":versions <name> - list all versions of a binding"
   T.putStrLn "<expr> - Evaluate <expr>, returning it's simplified form and type"
   T.putStrLn ":quit - give up and leave"
-  pure env
-doReplAction env (ListBindings) = do
-  let showBind = \(name, (StoreExpression _ expr)) -> T.putStrLn $ case getTypecheckedStoreExpression env expr of
-        Right (type', _, _, _) ->
-          prettyPrint name <> " :: " <> prettyPrint type'
-        _ -> ""
-  _ <- traverse showBind (getExprPairs (store env) (getCurrentBindings $ bindings env))
-  pure env
-doReplAction env Tui = do
-  goTui env
-doReplAction env (Versions name) = do
+
+----------
+
+doBind :: Project -> Name -> Expr Name -> IO Project
+doBind env name expr = case getTypecheckedStoreExpression env expr of
+  Left e' -> do
+    T.putStrLn (prettyPrint e')
+    pure env
+  Right (type', storeExpr, _, _) -> do
+    hash <- saveExpr storeExpr
+    T.putStrLn $
+      "Bound " <> prettyPrint name <> " to " <> prettyPrint expr
+        <> " :: "
+        <> prettyPrint type'
+    let newEnv = fromItem name storeExpr hash
+    pure (env <> newEnv)
+
+-------
+
+doInfo :: Project -> Expr Name -> IO ()
+doInfo env expr =
+  case getTypecheckedStoreExpression env expr of
+    Left e' -> do
+      T.putStrLn $ prettyPrint e'
+    Right (type', _, _, _) -> do
+      T.putStrLn $
+        prettyPrint expr
+          <> " :: "
+          <> prettyPrint type'
+
+----------
+
+doTree :: Project -> Expr Name -> IO ()
+doTree env expr = case getTypecheckedStoreExpression env expr of
+  Left e' -> do
+    T.putStrLn (prettyPrint e')
+  Right (_, storeExpr, _, _) -> do
+    let graph = createDepGraph (mkName "expression") (store env) storeExpr
+    T.putStrLn (prettyPrint graph)
+
+-------
+
+doVersions :: Project -> Name -> IO ()
+doVersions env name =
   case findVersions env name of
     Right versions ->
       let showIt (i, mt, expr', usages) = do
@@ -59,8 +118,42 @@ doReplAction env (Versions name) = do
        in traverse_ showIt versions
     Left e -> do
       T.putStrLn (prettyPrint e)
-  pure env
-doReplAction env Watch = do
+
+------
+
+doListBindings :: Project -> IO ()
+doListBindings env = do
+  let showBind = \(name, (StoreExpression _ expr)) ->
+        T.putStrLn $ case getTypecheckedStoreExpression env expr of
+          Right (type', _, _, _) ->
+            prettyPrint name <> " :: " <> prettyPrint type'
+          _ -> ""
+  traverse_ showBind (getExprPairs (store env) (getCurrentBindings $ bindings env))
+
+----------
+
+doEvaluate :: Project -> Expr Name -> IO Project
+doEvaluate env expr = case getTypecheckedStoreExpression env expr of
+  Left e' -> do
+    T.putStrLn $ prettyPrint e'
+    pure env
+  Right (type', _, expr', scope') -> do
+    simplified <- interpret scope' expr'
+    case simplified of
+      Left e -> do
+        T.putStrLn (prettyPrint e)
+        pure env
+      Right simplified' -> do
+        T.putStrLn $
+          prettyPrint simplified'
+            <> " :: "
+            <> prettyPrint type'
+        pure env
+
+---------
+
+doWatch :: Project -> IO ()
+doWatch env =
   watchFile
     "./store/"
     ( do
@@ -87,55 +180,3 @@ doReplAction env Watch = do
           Left e -> do
             T.putStrLn e
     )
-  pure env
-doReplAction env (Evaluate expr) = do
-  case getTypecheckedStoreExpression env expr of
-    Left e' -> do
-      T.putStrLn $ prettyPrint e'
-      pure env
-    Right (type', _, expr', scope') -> do
-      simplified <- interpret scope' expr'
-      case simplified of
-        Left e -> do
-          T.putStrLn (prettyPrint e)
-          pure env
-        Right simplified' -> do
-          T.putStrLn $
-            prettyPrint simplified'
-              <> " :: "
-              <> prettyPrint type'
-          pure env
-doReplAction env (Tree expr) = do
-  case getTypecheckedStoreExpression env expr of
-    Left e' -> do
-      T.putStrLn (prettyPrint e')
-      pure env
-    Right (_, storeExpr, _, _) -> do
-      let graph = createDepGraph (mkName "expression") (store env) storeExpr
-      T.putStrLn (prettyPrint graph)
-      pure env
-doReplAction env (Info expr) = do
-  case getTypecheckedStoreExpression env expr of
-    Left e' -> do
-      T.putStrLn $ prettyPrint e'
-      pure env
-    Right (type', _, _, _) -> do
-      T.putStrLn $
-        prettyPrint expr
-          <> " :: "
-          <> prettyPrint type'
-      pure env
-doReplAction env (Bind name expr) = do
-  case getTypecheckedStoreExpression env expr of
-    Left e' -> do
-      T.putStrLn (prettyPrint e')
-      pure env
-    Right (type', storeExpr, _, _) -> do
-      hash <- saveExpr storeExpr
-      T.putStrLn $
-        "Bound " <> prettyPrint name <> " to " <> prettyPrint expr
-          <> " :: "
-          <> prettyPrint type'
-      let newEnv = fromItem name storeExpr hash
-      pure (env <> newEnv)
-----------
