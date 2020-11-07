b0fa69acbd7df238d70ec8c3182ea2110a793a84
Daniel Harvey <danieljamesharvey@gmail.com>
2020-11-07 11:07:58 +0000

    Return only useful stuff when evaluating

--- src/Language/Mimsa/Server/Project.hs
+++ src/Language/Mimsa/Server/Project.hs
@@ -11,40 +11,47 @@ where
 
 import Control.Monad.Except
 import qualified Data.Aeson as JSON
-import Data.Functor
 import Data.Text (Text)
-import qualified Data.Text.IO as T
 import Data.Text.Lazy (fromStrict)
 import Data.Text.Lazy.Encoding (encodeUtf8)
 import GHC.Generics
 import Language.Mimsa.Actions (evaluateText)
 import Language.Mimsa.Printer
+import Language.Mimsa.Store
 import Language.Mimsa.Types.AST
 import Language.Mimsa.Types.Project
 import Language.Mimsa.Types.ResolvedExpression
+import Language.Mimsa.Types.Store
 import Servant
 
-newtype EvaluateBody
-  = EvaluateBody {code :: Text}
+newtype EvaluateRequest
+  = EvaluateRequest {code :: Text}
   deriving (Eq, Ord, Show, Generic, JSON.FromJSON)
 
+data EvaluateResponse
+  = EvaluateResponse {prettyExpr :: Text, prettyType :: Text, prettyHash :: Text}
+  deriving (Eq, Ord, Show, Generic, JSON.ToJSON)
+
 type ProjectAPI =
-  "project" :> ("evaluate" :> ReqBody '[JSON] EvaluateBody :> Post '[JSON] (ResolvedExpression ()))
+  "project" :> ("evaluate" :> ReqBody '[JSON] EvaluateRequest :> Post '[JSON] EvaluateResponse)
 
 projectEndpoints :: Project Annotation -> Server ProjectAPI
 projectEndpoints = evaluateExpression
 
 evaluateExpression ::
   Project Annotation ->
-  EvaluateBody ->
-  Handler (ResolvedExpression ())
+  EvaluateRequest ->
+  Handler EvaluateResponse
 evaluateExpression project body = do
-  liftIO $ T.putStrLn (code body)
   case evaluateText project (code body) of
-    Left e -> throwError (to400Error e)
-    Right a -> pure (a $> ())
+    Left e -> throwError (to500Error e)
+    Right (ResolvedExpression mt se _ _ _) -> do
+      let prettyExpr' = prettyPrint (storeExpression se)
+          prettyType' = prettyPrint mt
+          prettyHash' = prettyPrint (getStoreExpressionHash se)
+      pure (EvaluateResponse prettyExpr' prettyType' prettyHash')
 
-to400Error :: (Printer a) => a -> ServerError
-to400Error a = err400 {errBody = buildMsg a}
+to500Error :: (Printer a) => a -> ServerError
+to500Error a = err500 {errBody = buildMsg a}
   where
     buildMsg = encodeUtf8 . fromStrict . prettyPrint
