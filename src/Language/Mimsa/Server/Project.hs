e8fd2f8575e9025aecf6e81af502b35c63c1bc76
Daniel Harvey <danieljamesharvey@gmail.com>
2020-11-12 22:22:13 +0000

    Bind expression

--- src/Language/Mimsa/Server/Project.hs
+++ src/Language/Mimsa/Server/Project.hs
@@ -27,6 +27,22 @@ import Language.Mimsa.Types.ResolvedExpression
 import Language.Mimsa.Types.Store
 import Servant
 
+-----
+
+outputBindings :: Project a -> Map Name Text
+outputBindings project =
+  prettyPrint
+    <$> getBindings
+      ( getCurrentBindings
+          (bindings project)
+      )
+
+outputTypeBindings :: Project a -> Map TyCon Text
+outputTypeBindings project =
+  prettyPrint
+    <$> getTypeBindings
+      (getCurrentTypeBindings (typeBindings project))
+
 -- the Project endpoints output data in a way that front ends would be
 -- interested
 
@@ -36,14 +52,18 @@ type ProjectAPI =
   "project"
     :> ( EvaluateAPI :<|> ListBindings :<|> GetExpression
            :<|> CreateProject
+           :<|> BindExpression
        )
 
-projectEndpoints :: Project Annotation -> Server ProjectAPI
+projectEndpoints ::
+  Project Annotation ->
+  Server ProjectAPI
 projectEndpoints prj =
   evaluateExpression
     :<|> listBindings prj
     :<|> getExpression prj
     :<|> createProject prj
+    :<|> bindExpression
 
 -- /project/evaluate/
 
@@ -99,18 +119,11 @@ data BindingsResponse
 listBindings ::
   Project Annotation ->
   Handler BindingsResponse
-listBindings project = pure $ BindingsResponse values types
-  where
-    values =
-      prettyPrint
-        <$> getBindings
-          ( getCurrentBindings
-              (bindings project)
-          )
-    types =
-      prettyPrint
-        <$> getTypeBindings
-          (getCurrentTypeBindings (typeBindings project))
+listBindings project =
+  pure $
+    BindingsResponse
+      (outputBindings project)
+      (outputTypeBindings project)
 
 -- /project/expression/
 
@@ -129,7 +142,10 @@ data ExpressionResponse
       }
   deriving (Eq, Ord, Show, Generic, JSON.ToJSON)
 
-getExpression :: Project Annotation -> ExprHash -> Handler ExpressionResponse
+getExpression ::
+  Project Annotation ->
+  ExprHash ->
+  Handler ExpressionResponse
 getExpression project exprHash' = do
   se <- handleEither InternalError $
     case lookupExprHash project exprHash' of
@@ -159,16 +175,55 @@ data CreateProjectResponse
   deriving (Eq, Ord, Show, Generic, JSON.ToJSON)
 
 -- creating a project in this case means copying the default one
-createProject :: Project Annotation -> Handler CreateProjectResponse
-createProject project = do
-  let values =
-        prettyPrint
-          <$> getBindings
-            ( getCurrentBindings
-                (bindings project)
-            )
-      types =
-        prettyPrint
-          <$> getTypeBindings
-            (getCurrentTypeBindings (typeBindings project))
-  pure $ CreateProjectResponse project values types
+createProject ::
+  Project Annotation ->
+  Handler CreateProjectResponse
+createProject project =
+  pure $
+    CreateProjectResponse
+      project
+      (outputBindings project)
+      (outputTypeBindings project)
+
+------
+
+type BindExpression =
+  "bind"
+    :> ReqBody '[JSON] BindExpressionRequest
+    :> Post '[JSON] BindExpressionResponse
+
+data BindExpressionRequest
+  = BindExpressionRequest
+      { beProject :: Project Annotation,
+        beBindingName :: Name,
+        beExpression :: Text
+      }
+  deriving (Eq, Ord, Show, Generic, JSON.FromJSON)
+
+data BindExpressionResponse
+  = BindExpressionResponse
+      { beData :: Project Annotation,
+        beBindings :: Map Name Text,
+        beTypeBindings :: Map TyCon Text,
+        bePrettyExpr :: Text,
+        bePrettyType :: Text,
+        bePrettyHash :: Text
+      }
+  deriving (Eq, Ord, Show, Generic, JSON.ToJSON)
+
+bindExpression ::
+  BindExpressionRequest ->
+  Handler BindExpressionResponse
+bindExpression (BindExpressionRequest project name' input) = do
+  (ResolvedExpression mt se _ _ _) <-
+    handleEither UserError (evaluateText project input)
+  hash <- handleExceptT InternalError (saveExpr se)
+  let newEnv = project <> fromItem name' se hash
+  pure $
+    BindExpressionResponse
+      newEnv
+      (outputBindings newEnv)
+      (outputTypeBindings newEnv)
+      (prettyPrint (storeExpression se))
+      (prettyPrint mt)
+      (prettyPrint hash)
