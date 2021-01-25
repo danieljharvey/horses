78c08c9434904c94535ec2da4f7fce68118cc486
Daniel Harvey <danieljamesharvey@gmail.com>
2021-01-25 08:39:00 +0000

    Use Evaluate action in repl

--- src/Language/Mimsa/Repl/Actions/Evaluate.hs
+++ src/Language/Mimsa/Repl/Actions/Evaluate.hs
@@ -5,30 +5,21 @@ module Language.Mimsa.Repl.Actions.Evaluate
   )
 where
 
-import Data.Bifunctor (first)
 import Data.Text (Text)
-import Language.Mimsa.Actions
-import Language.Mimsa.Interpreter (interpret)
-import Language.Mimsa.Printer
+import qualified Language.Mimsa.Actions.Evaluate as Actions
+import Language.Mimsa.Repl.Helpers
 import Language.Mimsa.Repl.Types
 import Language.Mimsa.Types.AST
-import Language.Mimsa.Types.Error
 import Language.Mimsa.Types.Identifiers
 import Language.Mimsa.Types.Project
-import Language.Mimsa.Types.ResolvedExpression
 
 doEvaluate ::
   Project Annotation ->
   Text ->
   Expr Name Annotation ->
   ReplM Annotation ()
-doEvaluate env input expr = do
-  (ResolvedExpression type' _ expr' scope' swaps) <-
-    liftRepl $ getTypecheckedStoreExpression input env expr
-  simplified <- liftRepl (first InterpreterErr (interpret scope' swaps expr'))
-  replPrint $
-    prettyPrint simplified
-      <> "\n::\n"
-      <> prettyPrint type'
+doEvaluate project input expr = do
+  _ <- toReplM project (Actions.evaluate input expr)
+  pure ()
 
 ---------
