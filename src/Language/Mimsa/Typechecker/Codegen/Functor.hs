cb027172170454334d2e42111f40f66717601e79
Daniel Harvey <danieljamesharvey@gmail.com>
2021-02-07 09:15:00 +0000

    Recurses lists of many type vars

--- src/Language/Mimsa/Typechecker/Codegen/Functor.hs
+++ src/Language/Mimsa/Typechecker/Codegen/Functor.hs
@@ -6,6 +6,7 @@ module Language.Mimsa.Typechecker.Codegen.Functor
   )
 where
 
+import Data.Coerce
 import Data.Foldable (foldl')
 import qualified Data.List.NonEmpty as NE
 import Data.Map (Map)
@@ -29,23 +30,28 @@ functorMap (DataType tyCon vars items) = do
       matches <-
         traverse
           ( \(consName, fields) ->
-              (,) consName <$> createMatch fVar consName fields
+              (,) consName <$> createMatch tyCon fVar consName fields
           )
           constructors
       pure
-        ( MyLambda
+        ( MyLet
             mempty
-            "f"
+            "fmap"
             ( MyLambda
                 mempty
-                tyName
-                ( MyCaseMatch
+                "f"
+                ( MyLambda
                     mempty
-                    (MyVar mempty tyName)
-                    matches
-                    Nothing
+                    tyName
+                    ( MyCaseMatch
+                        mempty
+                        (MyVar mempty tyName)
+                        matches
+                        Nothing
+                    )
                 )
             )
+            (MyVar mempty "fmap")
         )
 
 getFunctorVar :: [Name] -> Either Text Name
@@ -53,37 +59,61 @@ getFunctorVar names = case NE.nonEmpty names of
   Just neNames -> Right $ NE.last neNames
   _ -> Left "Type should have at least one type variable"
 
+data FieldItemType
+  = VariableField Name
+  | RecurseField Name
+
+toFieldItemType :: TyCon -> Field -> Either Text FieldItemType
+toFieldItemType typeName = \case
+  VarName a -> Right (VariableField a)
+  ConsName fieldConsName _fields
+    | fieldConsName == typeName ->
+      Right (RecurseField (restName typeName))
+  _ -> Left "Expected VarName"
+
+restName :: TyCon -> Name
+restName tyCon = "rest" <> coerce tyCon
+
 createMatch ::
+  TyCon ->
   Name ->
   TyCon ->
   [Field] ->
   Either Text (Expr Name ())
-createMatch matchVar tyCon fields = do
+createMatch typeName matchVar tyCon fields = do
   regFields <-
-    traverse
-      ( \case
-          VarName a -> Right a
-          _ -> Left "Expected VarName"
-      )
-      fields
+    traverse (toFieldItemType typeName) fields
   let withConsApp =
         foldl'
-          ( \expr' varName ->
-              let var =
-                    if varName == matchVar
-                      then MyApp mempty (MyVar mempty "f") (MyVar mempty varName)
-                      else MyVar mempty varName
-               in MyConsApp mempty expr' var
+          ( \expr' field -> case field of
+              VariableField varName ->
+                let var =
+                      if varName == matchVar
+                        then MyApp mempty (MyVar mempty "f") (MyVar mempty varName)
+                        else MyVar mempty varName
+                 in MyConsApp mempty expr' var
+              RecurseField restVar ->
+                let var =
+                      MyApp
+                        mempty
+                        ( MyApp
+                            mempty
+                            (MyVar mempty "fmap")
+                            (MyVar mempty "f")
+                        )
+                        (MyVar mempty restVar)
+                 in MyConsApp mempty expr' var
           )
           (MyConstructor mempty tyCon)
           regFields
-      expression =
-        foldr
-          ( MyLambda mempty
-          )
-          withConsApp
-          regFields
-  pure expression
+  pure $
+    foldr
+      ( \field expr' -> case field of
+          VariableField n -> MyLambda mempty n expr'
+          RecurseField restVar -> MyLambda mempty restVar expr'
+      )
+      withConsApp
+      regFields
 
 getMapItems :: Map k a -> Maybe (NE.NonEmpty (k, a))
 getMapItems = NE.nonEmpty . M.toList
