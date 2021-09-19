4228bc5c3b08864e454b81a0ce27730c005aca3e
Daniel Harvey <danieljamesharvey@gmail.com>
2021-09-19 15:31:24 +0100

    Stop invalid names being created

--- ui/src/components/Editor/utils/validateBindingName.ts
+++ ui/src/components/Editor/utils/validateBindingName.ts
@@ -1,38 +1,45 @@
 import * as E from 'fp-ts/Either'
 
 export type ValidationError =
-    | { type: 'EmptyName' }
-    | { type: 'AlreadyExists'; existing: string }
-    | { type: 'EmptyExpression' }
+  | { type: 'EmptyName' }
+  | { type: 'InvalidName' }
+  | { type: 'AlreadyExists'; existing: string }
+  | { type: 'EmptyExpression' }
 
 export const validateBinding = (
-    str: string,
-    code: string,
-    bindings: string[]
+  str: string,
+  code: string,
+  bindings: string[]
 ): E.Either<ValidationError, string> => {
-    const cleanString = str.trim()
-    if (cleanString.length < 1) {
-        return E.left({ type: 'EmptyName' })
-    }
-    if (bindings.includes(cleanString)) {
-        return E.left({
-            type: 'AlreadyExists',
-            existing: cleanString,
-        })
-    }
-    if (code.length < 1) {
-        return E.left({ type: 'EmptyExpression' })
-    }
-    return E.right(cleanString)
+  const cleanString = str.trim()
+  if (cleanString.length < 1) {
+    return E.left({ type: 'EmptyName' })
+  }
+  const firstChar = cleanString.charAt(0)
+  if (firstChar !== firstChar.toLowerCase()) {
+    return E.left({ type: 'InvalidName' })
+  }
+  if (bindings.includes(cleanString)) {
+    return E.left({
+      type: 'AlreadyExists',
+      existing: cleanString,
+    })
+  }
+  if (code.length < 1) {
+    return E.left({ type: 'EmptyExpression' })
+  }
+  return E.right(cleanString)
 }
 
 export const showError = (err: ValidationError): string => {
-    switch (err.type) {
-        case 'EmptyName':
-            return 'Name must be non-empty'
-        case 'AlreadyExists':
-            return `${err.existing} already exists!`
-        case 'EmptyExpression':
-            return 'Expression must not be empty'
-    }
+  switch (err.type) {
+    case 'EmptyName':
+      return 'Name must be non-empty'
+    case 'AlreadyExists':
+      return `${err.existing} already exists!`
+    case 'EmptyExpression':
+      return 'Expression must not be empty'
+    case 'InvalidName':
+      return 'Identifier must start with a lower-case letter'
+  }
 }
