3a58cadbd52085eeb60506c9a60b155385ac02c5
Daniel Harvey <danieljamesharvey@gmail.com>
2021-09-20 16:01:20 +0100

    Add existing deps to project when re-binding expression

--- compiler/src/Language/Mimsa/Actions/BindExpression.hs
+++ compiler/src/Language/Mimsa/Actions/BindExpression.hs
@@ -4,6 +4,7 @@ module Language.Mimsa.Actions.BindExpression (bindExpression) where
 
 import Control.Monad.Except (liftEither)
 import Data.Foldable (traverse_)
+import qualified Data.Map as M
 import Data.Text (Text)
 import qualified Language.Mimsa.Actions.Graph as Actions
 import qualified Language.Mimsa.Actions.Monad as Actions
@@ -14,6 +15,7 @@ import Language.Mimsa.Project.UnitTest
 import Language.Mimsa.Store
 import Language.Mimsa.Types.AST
 import Language.Mimsa.Types.Identifiers
+import Language.Mimsa.Types.Project
 import Language.Mimsa.Types.ResolvedExpression
 import Language.Mimsa.Types.Store
 
@@ -26,12 +28,21 @@ bindExpression ::
   Actions.ActionM (ExprHash, Int, ResolvedExpression Annotation, [Graphviz])
 bindExpression expr name input = do
   project <- Actions.getProject
+  -- if there is an existing binding of this name, bring its deps into scope
+  -- perhaps this should be more specific and actually take the previous
+  -- exprHash as an argument
+  let project' =
+        project
+          <> maybe
+            mempty
+            fromStoreExpressionDeps
+            (getExistingBinding name project)
   resolvedExpr <-
-    liftEither $ Actions.getTypecheckedStoreExpression input project expr
+    liftEither $ Actions.getTypecheckedStoreExpression input project' expr
   let storeExpr = reStoreExpression resolvedExpr
   Actions.bindStoreExpression storeExpr name
   graphviz <- Actions.graphExpression storeExpr
-  case lookupBindingName project name of
+  case lookupBindingName project' name of
     Nothing -> do
       Actions.appendMessage
         ( "Bound " <> prettyPrint name <> "."
@@ -47,6 +58,11 @@ bindExpression expr name input = do
           <$> createUnitTests oldExprHash newExprHash
             <*> pure resolvedExpr
             <*> pure graphviz
+
+getExistingBinding :: Name -> Project Annotation -> Maybe (StoreExpression Annotation)
+getExistingBinding name prj =
+  lookupBindingName prj name
+    >>= \exprHash -> M.lookup exprHash (getStore $ prjStore prj)
 
 createUnitTests :: ExprHash -> ExprHash -> Actions.ActionM Int
 createUnitTests oldExprHash newExprHash = do
