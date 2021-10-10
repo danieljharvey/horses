9272004e31d5adc48f160abf3dc95f65c849bfb0
Daniel Harvey <danieljamesharvey@gmail.com>
2021-10-10 15:25:35 +0100

    Remove temp test files

--- compiler/test/golden/ESModules/-1117335091141931317.mjs
+++
@@ -1,8 +0,0 @@
-import { __eq, __concat, __patternMatch } from '../../../static/backend/es-modules-js/stdlib.mjs'
-const id = a => a
-const aRecord = {a:100}
-const aPair = [1,2]
-export const main = function() { const { cat: b, dog: a } = { cat: 2, dog: 1 };
-return [a,b] }();
-
-console.log(main)
--- compiler/test/golden/ESModules/-7723579357268470197.mjs
+++
@@ -1,9 +0,0 @@
-import { __eq, __concat, __patternMatch } from '../../../static/backend/es-modules-js/stdlib.mjs'
-const id = a => a
-const aRecord = {a:100}
-const aPair = [1,2]
-export const main = function() { const a = "dog";
-const b = "horse";
-return 123 }();
-
-console.log(main)
