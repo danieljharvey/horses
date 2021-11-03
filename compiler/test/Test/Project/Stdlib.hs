4e39f3f0dd6c0bbb3570c2baf1621264ae9cdda7
Daniel Harvey <danieljamesharvey@gmail.com>
2021-11-03 11:48:50 +0000

    Fix stdlib and add test (#340)

--- compiler/test/Test/Project/Stdlib.hs
+++ compiler/test/Test/Project/Stdlib.hs
@@ -5,12 +5,42 @@ module Test.Project.Stdlib
   )
 where
 
+import Data.Coerce
 import Data.Either
+import Data.Foldable
+import qualified Data.Map as M
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Language.Mimsa.Actions.Shared as Actions
+import Language.Mimsa.Interpreter
+import Language.Mimsa.Printer
 import Language.Mimsa.Project.Stdlib
+import Language.Mimsa.Types.AST
+import Language.Mimsa.Types.Identifiers
+import Language.Mimsa.Types.Project
+import Language.Mimsa.Types.ResolvedExpression
 import Test.Hspec
 
+evaluatesSuccessfully ::
+  Project Annotation ->
+  Text ->
+  Spec
+evaluatesSuccessfully env input =
+  it ("Evaluates " <> T.unpack input <> " from stdlib") $ do
+    case Actions.evaluateText env input of
+      Left e -> error (T.unpack (prettyPrint e))
+      Right (ResolvedExpression _ _ expr' scope' swaps _ _) -> do
+        let endExpr = interpret scope' swaps expr'
+        endExpr `shouldSatisfy` isRight
+
 spec :: Spec
-spec =
+spec = do
   describe "Stdlib" $ do
-    it "Builds" $
+    it "Builds" $ do
       buildStdlib `shouldSatisfy` isRight
+  describe "Can evaluate each top-level item" $ do
+    case buildStdlib of
+      Right prj ->
+        let bindingNames = M.keys . getVersionedMap . prjBindings $ prj
+         in traverse_ (evaluatesSuccessfully prj . coerce) bindingNames
+      Left e -> error (T.unpack (prettyPrint e))
