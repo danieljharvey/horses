a285e18de1fffadbae9a50325a6bb331a95b63d2
Daniel Harvey <danieljamesharvey@gmail.com>
2021-12-11 21:14:28 +0000

    Complete swap name

--- compiler/src/Language/Mimsa/Interpreter/SwapName.hs
+++ compiler/src/Language/Mimsa/Interpreter/SwapName.hs
@@ -24,16 +24,61 @@ swapName from to (MyLetPattern ann pat expr body) =
 swapName from to expr =
   mapExpr (swapName from to) expr
 
+-- | TODO: recurse through all cases
 swapPatternName ::
   Variable ->
   Variable ->
   Pattern Variable ann ->
   Pattern Variable ann
-swapPatternName from to (PVar ann from') =
+swapPatternName from to pat =
+  case pat of
+    (PVar ann from') ->
+      if from == from'
+        then PVar ann to
+        else PVar ann from'
+    (PWildcard ann) ->
+      PWildcard ann
+    (PLit ann l) -> PLit ann l
+    (PConstructor ann c d) -> PConstructor ann c d
+    (PPair ann a b) ->
+      PPair
+        ann
+        (swapPatternName from to a)
+        (swapPatternName from to b)
+    (PRecord ann as) ->
+      PRecord ann (swapPatternName from to <$> as)
+    (PArray ann as a) ->
+      PArray
+        ann
+        (swapPatternName from to <$> as)
+        (swapSpreadName from to a)
+    (PString ann as a) ->
+      PString
+        ann
+        (swapStringPartName from to as)
+        (swapStringPartName from to a)
+
+swapSpreadName ::
+  Variable ->
+  Variable ->
+  Spread Variable ann ->
+  Spread Variable ann
+swapSpreadName from to (SpreadValue ann from') =
   if from == from'
-    then PVar ann to
-    else PVar ann from'
-swapPatternName _ _ other = other
+    then SpreadValue ann to
+    else SpreadValue ann from'
+swapSpreadName _ _ a = a
+
+swapStringPartName ::
+  Variable ->
+  Variable ->
+  StringPart Variable ann ->
+  StringPart Variable ann
+swapStringPartName from to (StrValue ann from') =
+  if from == from'
+    then StrValue ann to
+    else StrValue ann from'
+swapStringPartName _ _ a = a
 
 swapInIdent :: Variable -> Variable -> Identifier Variable ann -> Identifier Variable ann
 swapInIdent from to (Identifier ann from') =
