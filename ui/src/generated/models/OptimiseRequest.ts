9f902c697baf042bede7293af0d07e57d954ad8c
Daniel Harvey <danieljamesharvey@gmail.com>
2022-01-28 14:35:14 +0000

    Regenerate types

---
+++ ui/src/generated/models/OptimiseRequest.ts
@@ -0,0 +1,8 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type OptimiseRequest = {
+  upProjectHash: string
+  upBindingName: string
+}
---
+++ ui/src/generated/models/OptimiseResponse.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { ExpressionData } from './ExpressionData'
+import type { ProjectData } from './ProjectData'
+
+export type OptimiseResponse = {
+  upExpressionData: ExpressionData
+  upProjectData: ProjectData
+}
