640dce71bb85101fa4eec3e8c30e660557cd9758
Daniel Harvey <danieljamesharvey@gmail.com>
2022-02-14 10:46:01 +0000

    Types for get expressiona

--- compiler/server/Server/Project/GetExpression.hs
+++ compiler/server/Server/Project/GetExpression.hs
@@ -13,6 +13,7 @@ where
 import qualified Data.Aeson as JSON
 import Data.OpenApi
 import GHC.Generics
+import qualified Language.Mimsa.Actions.Helpers.Parse as Actions
 import qualified Language.Mimsa.Actions.Graph as Actions
 import qualified Language.Mimsa.Actions.Helpers.CanOptimise as Actions
 import Language.Mimsa.Transform.Warnings
@@ -23,6 +24,7 @@ import Servant
 import Server.Handlers
 import Server.Helpers.ExpressionData
 import Server.Types
+import Language.Mimsa.Printer
 
 -- /project/expression/
 
@@ -53,21 +55,30 @@ getExpression mimsaEnv (GetExpressionRequest projectHash exprHash') = do
   store' <- readStoreHandler mimsaEnv
   project <- loadProjectHandler mimsaEnv store' projectHash
   se <- findExprHandler project exprHash'
+
   let action =
-        (,) <$> Actions.graphExpression se <*> Actions.canOptimise se
-  (_, (graphviz, canOptimise)) <-
+        (,,) <$> Actions.graphExpression se <*> Actions.canOptimise se
+          <*> Actions.parseExpr (prettyPrint (storeExpression se))
+
+  (_, (graphviz, canOptimise, exprName)) <-
     fromActionM
       mimsaEnv
       projectHash
       action
+
+  -- re-resolved expr, using the parsed input expr (as it has annotations)
   resolvedExpr <-
-    resolveStoreExpressionHandler project se
+    resolveStoreExpressionHandler project (se {storeExpression = exprName})
+
   writeStoreHandler mimsaEnv (prjStore project)
+
+  -- | turn Expr Variable MonoType into Expr Name MonoType
   typedExpr <-
     useSwapsHandler
       (reSwaps resolvedExpr)
       (reTypedExpression resolvedExpr)
   let warnings = getWarnings resolvedExpr
+
   pure $
     GetExpressionResponse
       (makeExpressionData se typedExpr graphviz (reInput resolvedExpr) warnings canOptimise)
