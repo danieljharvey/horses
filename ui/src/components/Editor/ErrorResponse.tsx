9877aa35ce1dc419aeefc220ee02dca5ba47efce
Daniel Harvey <danieljamesharvey@gmail.com>
2022-03-02 21:00:38 +0000

    Split result component

---
+++ ui/src/components/Editor/ErrorResponse.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+
+import { Paragraph } from '../View/Paragraph'
+import { FlexColumnSpaced } from '../View/FlexColumnSpaced'
+import { UserErrorResponse } from '../../types'
+
+type Props = {
+  errorResponse: UserErrorResponse
+}
+
+export const ErrorResponse: React.FC<Props> = ({
+  errorResponse,
+}) => {
+  return (
+    <FlexColumnSpaced>
+      <Paragraph>{errorResponse.ueText}</Paragraph>
+      {errorResponse.ueTypedHoles.map((th) => (
+        <FlexColumnSpaced>
+          <Paragraph>{`${th.thName}: ${th.thMonoType}`}</Paragraph>
+          {th.thSuggestions.length > 0 && (
+            <Paragraph>
+              {`  Suggestions: ${th.thSuggestions.join(
+                ', '
+              )}`}
+            </Paragraph>
+          )}
+        </FlexColumnSpaced>
+      ))}
+    </FlexColumnSpaced>
+  )
+}
---
+++ ui/src/components/Editor/Expression.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+
+import { Code } from '../View/Code'
+import { Paragraph } from '../View/Paragraph'
+import { FlexColumnSpaced } from '../View/FlexColumnSpaced'
+import { ExpressionData } from '../../types'
+
+type Props = {
+  expression: ExpressionData
+}
+
+// show type and warnings of an expression
+export const Expression: React.FC<Props> = ({
+  expression,
+}) => {
+  return (
+    <FlexColumnSpaced>
+      <Code codeType="type">{expression.edType}</Code>
+      {expression.edWarnings.length > 0 && (
+        <FlexColumnSpaced>
+          {expression.edWarnings.map((warn) => (
+            <Paragraph>{warn}</Paragraph>
+          ))}
+        </FlexColumnSpaced>
+      )}
+    </FlexColumnSpaced>
+  )
+}
