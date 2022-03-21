990d5f35e7ea7cc22c6e00b3b04f2dee4a2f6d05
Daniel Harvey <danieljamesharvey@gmail.com>
2022-03-21 11:37:25 +0000

    Clear up (#574)

--- compiler/test/golden/ESModules/-5396306910030681641.mjs
+++
@@ -1,7 +0,0 @@
-import { __eq, __concat, __patternMatch } from '../../../static/backend/es-modules-js/stdlib.mjs'
-const id = a => a
-const aRecord = {a:100}
-const aPair = [1,2]
-export const main = a => ({ type: "Just", vars: [a] });
-
-console.log(main)
--- compiler/test/golden/ESModules/-9106303967038125043.mjs
+++
@@ -1,7 +0,0 @@
-import { __eq, __concat, __patternMatch } from '../../../static/backend/es-modules-js/stdlib.mjs'
-const id = a => a
-const aRecord = {a:100}
-const aPair = [1,2]
-export const main = aRecord.a;
-
-console.log(main)
