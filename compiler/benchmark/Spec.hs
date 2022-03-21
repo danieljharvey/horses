e005f9883d70e37e81bcb5dbe6ccddab60788a7a
Daniel Harvey <danieljamesharvey@gmail.com>
2022-03-21 10:31:11 +0000

    New benchmarks

--- compiler/benchmark/Spec.hs
+++ compiler/benchmark/Spec.hs
@@ -1,13 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main
   ( main,
   )
 where
 
 import Criterion.Main
+import Data.Functor
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Language.Mimsa.Actions.Compile as Actions
+import qualified Language.Mimsa.Actions.Evaluate as Actions
 import qualified Language.Mimsa.Actions.Monad as Actions
+import Language.Mimsa.Backend.Types
+import Language.Mimsa.Parser
+import Language.Mimsa.Printer
 import Language.Mimsa.Project.Stdlib
 import Language.Mimsa.Types.AST
+import Language.Mimsa.Types.Identifiers
 import Language.Mimsa.Types.Project
+
+unsafeParseExpr :: Text -> Expr Name ()
+unsafeParseExpr t = case parseExpr t of
+  Right a -> a $> ()
+  Left _ ->
+    error $
+      "Error parsing expr for benchmark:"
+        <> T.unpack t
 
 -- build the stdlib, exploding if it fails (so that doesn't look like an
 -- incredible speedup)
@@ -20,15 +39,27 @@ buildThing action prj =
     Right (proj, _, _) -> proj
     Left e -> error (show e)
 
-arrayFns' :: Actions.ActionM ()
-arrayFns' = do
-  baseFns
-  arrayFns
+-- compile something
+compileThing :: Text -> Project Annotation
+compileThing input =
+  let action = do
+        let expr = unsafeParseExpr input $> mempty
+        (_, _, storeExpr, _, _) <- Actions.evaluate (prettyPrint expr) expr
+        Actions.compile Typescript storeExpr
+   in case Actions.run stdlib action of
+        Right (proj, _, _) -> proj
+        Left e -> error (show e)
 
-nonEmptyArrayFns' :: Actions.ActionM ()
-nonEmptyArrayFns' = do
-  arrayFns'
-  nonEmptyArrayFns
+-- evaluate something
+evaluateThing :: Text -> Expr Name Annotation
+evaluateThing input =
+  let action = do
+        let expr = unsafeParseExpr input $> mempty
+        (_, result, _, _, _) <- Actions.evaluate (prettyPrint expr) expr
+        pure result
+   in case Actions.run stdlib action of
+        Right (_, _, res) -> res
+        Left e -> error (show e)
 
 -- Our benchmark harness.
 main :: IO ()
@@ -36,9 +67,16 @@ main =
   defaultMain
     [ bgroup
         "build stdlib"
-        [ bench "baseFns" $ whnf (buildThing baseFns) mempty,
-          bench "arrayFns" $ whnf (buildThing arrayFns') mempty,
-          bench "nonEmptyArrayFns" $ whnf (buildThing nonEmptyArrayFns') mempty,
-          bench "allFns" $ whnf (buildThing allFns) mempty
+        [ bench "allFns" $ whnf (buildThing allFns) mempty
+        ],
+      bgroup
+        "compilation"
+        [ bench "compile either fmap test" $
+            whnf compileThing "either.fmap (\\a -> a + 1) (Right 100)"
+        ],
+      bgroup
+        "evaluate"
+        [ bench "evaluate big looping thing" $
+            whnf evaluateThing "let countdown a = if a == 0 then True else countdown (a - 1); countdown 100000"
         ]
     ]
