fdc9031aceb50db9bcac7225d1e3fbbc970bad33
Daniel Harvey <danieljamesharvey@gmail.com>
2022-04-06 15:36:02 +0100

    Yes, strictness is faster (#608)

--- compiler/test/Test/Typechecker/DataTypes.hs
+++ compiler/test/Test/Typechecker/DataTypes.hs
@@ -8,7 +8,7 @@ where
 
 import Control.Monad.Except
 import Control.Monad.Identity
-import Control.Monad.State
+import Control.Monad.State.Strict
 import Language.Mimsa.Typechecker.DataTypes
 import Language.Mimsa.Typechecker.TcMonad
 import Language.Mimsa.Types.Error
