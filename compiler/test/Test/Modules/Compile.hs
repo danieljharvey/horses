d97732beb446e710a0170002dfe6ba1c6ee83ea2
Daniel Harvey <danieljamesharvey@gmail.com>
2022-06-04 10:45:13 +0100

    WIP 2

---
+++ compiler/test/Test/Modules/Compile.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Modules.Compile
+  ( spec,
+  )
+where
+
+import Data.Functor
+import qualified Data.Map as M
+import Language.Mimsa.Modules.Check
+import Language.Mimsa.Modules.Compile
+import Language.Mimsa.Modules.Monad
+import Language.Mimsa.Printer
+import Language.Mimsa.Store
+import Language.Mimsa.Types.AST
+import Language.Mimsa.Types.Modules.DefIdentifier
+import Language.Mimsa.Types.Modules.Module
+import Language.Mimsa.Types.Store
+import Test.Hspec
+import Test.Utils.Helpers
+
+compile' :: Module Annotation -> CompiledModule Annotation
+compile' mod' =
+  let action = do
+        tcMod <- typecheckAllModules mod'
+        compile tcMod
+   in fromRight $ runCheck (prettyPrint mod') mempty action
+
+spec :: Spec
+spec = do
+  describe "Compile modules" $ do
+    it "Empty module, no outputs" $ do
+      compile' mempty
+        `shouldBe` CompiledModule
+          { cmStore = mempty,
+            cmExprs = mempty
+          }
+    it "Single expression, single output with no deps" $ do
+      let expr = unsafeParseExpr "\\a -> a" $> mempty
+          storeExpr = StoreExpression expr mempty mempty
+          hash = getStoreExpressionHash storeExpr
+          inputModule =
+            mempty
+              { moExpressions = M.singleton (DIName "id") expr
+              }
+          expected =
+            CompiledModule
+              { cmStore = Store $ M.singleton hash storeExpr,
+                cmExprs = M.singleton (DIName "id") hash
+              }
+      compile' inputModule `shouldBe` expected
+    it "Two expressions, one depends on the other" $ do
+      let exprA = unsafeParseExpr "\\a -> a" $> mempty
+          storeExprA = StoreExpression exprA mempty mempty
+          hashA = getStoreExpressionHash storeExprA
+
+          exprB = unsafeParseExpr "id 100" $> mempty
+          storeExprB = StoreExpression exprB (Bindings $ M.singleton "id" hashA) mempty
+          hashB = getStoreExpressionHash storeExprB
+
+          inputModule =
+            mempty
+              { moExpressions =
+                  M.fromList
+                    [ (DIName "id", exprA),
+                      (DIName "useId", exprB)
+                    ]
+              }
+          expected =
+            CompiledModule
+              { cmStore =
+                  Store $
+                    M.fromList
+                      [ (hashA, storeExprA),
+                        (hashB, storeExprB)
+                      ],
+                cmExprs =
+                  M.fromList
+                    [ (DIName "id", hashA),
+                      (DIName "useId", hashB)
+                    ]
+              }
+      compile' inputModule `shouldBe` expected
