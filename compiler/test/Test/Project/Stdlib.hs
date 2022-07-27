8cf76f2fc6284c2ac9c7263cf421c5de7bf29c6b
Daniel Harvey <danieljamesharvey@gmail.com>
2022-07-27 13:38:12 +0100

    Test all modules compile

--- compiler/test/Test/Project/Stdlib.hs
+++ compiler/test/Test/Project/Stdlib.hs
@@ -12,12 +12,16 @@ import Data.Foldable
 import qualified Data.Map as M
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Language.Mimsa.Actions.Helpers.LookupExpression as Actions
 import qualified Language.Mimsa.Actions.Interpret as Actions
+import qualified Language.Mimsa.Actions.Modules.Check as Actions
 import qualified Language.Mimsa.Actions.Monad as Actions
 import Language.Mimsa.Printer
+import Language.Mimsa.Project.Helpers
 import Language.Mimsa.Project.Stdlib
 import Language.Mimsa.Types.AST
 import Language.Mimsa.Types.Identifiers
+import Language.Mimsa.Types.Modules
 import Language.Mimsa.Types.Project
 import Language.Mimsa.Types.ResolvedExpression
 import Test.Hspec
@@ -27,13 +31,24 @@ evaluatesSuccessfully ::
   Project Annotation ->
   Text ->
   Spec
-evaluatesSuccessfully env input =
+evaluatesSuccessfully project input =
   it ("Evaluates " <> T.unpack input <> " from stdlib") $ do
-    case evaluateText env input of
+    case evaluateText project input of
       Left e -> error (T.unpack (prettyPrint e))
       Right resolved -> do
-        Actions.run env (Actions.interpreter (reStoreExpression resolved))
+        Actions.run project (Actions.interpreter (reStoreExpression resolved))
           `shouldSatisfy` isRight
+
+moduleTypechecksSuccessfully ::
+  Project Annotation ->
+  (ModuleName, ModuleHash) ->
+  Spec
+moduleTypechecksSuccessfully project (modName, modHash) =
+  it ("Typechecks module " <> T.unpack (prettyPrint modName) <> " from stdlib") $ do
+    let action = do
+          thisMod <- Actions.lookupModule modHash
+          Actions.checkModule (prjModuleStore project) (prettyPrint thisMod)
+    Actions.run project action `shouldSatisfy` isRight
 
 spec :: Spec
 spec = do
@@ -46,6 +61,16 @@ spec = do
       Right prj ->
         let bindingNames = M.keys . getVersionedMap . prjBindings $ prj
          in traverse_ (evaluatesSuccessfully prj . coerce) bindingNames
+      Left e ->
+        it "could not create stdlib" $ do
+          liftIO (putStrLn (T.unpack (prettyPrint e)))
+          False `shouldBe` True
+
+  describe "Can typecheck each top-level module" $ do
+    case buildStdlib of
+      Right prj ->
+        let moduleNames = M.toList . getCurrentModules . prjModules $ prj
+         in traverse_ (moduleTypechecksSuccessfully prj) moduleNames
       Left e ->
         it "could not create stdlib" $ do
           liftIO (putStrLn (T.unpack (prettyPrint e)))
