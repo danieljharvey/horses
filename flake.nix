f1de99f04584fd040879135a6a4eb9bc32a5713d
Daniel Harvey <danieljamesharvey@gmail.com>
2022-12-19 13:15:03 +0000

    Nix flake (#842)
    * Well, well, well
    
    * Rename
    
    * yes
    
    * Probably fine:
    
    * Bin off .direnv
    
    * Fixed vaguely I think

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Mimsa";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        
+        compilerVersion = "ghc925";
+
+        # fix things
+        haskell = pkgs.haskell // {
+          packages = pkgs.haskell.packages // {
+            "${compilerVersion}" =
+              pkgs.haskell.packages."${compilerVersion}".override {
+                overrides = self: super: {
+                  # On aarch64-darwin, this creates a cycle for some reason; didn't look too much into it.
+                  ghcid = pkgs.haskell.lib.overrideCabal super.ghcid (drv: { enableSeparateBinOutput = false; });
+                };
+
+              };
+          };
+        };
+
+        haskellPackages = haskell.packages.${compilerVersion};
+
+        jailbreakUnbreak = pkg:
+          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+
+        packageName = "mimsa";
+      in {
+        # we're not interested in building with Nix, just using it for deps
+        packages.${system}.${packageName} = {};
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with haskellPackages; [
+            ghc
+            hlint
+            ormolu
+            ghcid
+            cabal-install
+            pkgs.zlib # used by `digest` package
+            pkgs.nodejs-18_x
+          ];
+
+          # put clang_14 on the path
+          #shellHook = with pkgs; ''
+          #  export PATH="${clang_14}/bin:$PATH"
+          #'';
+
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
+}
+
+
--- format.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-HS_FILES=$(git ls-files '*.hs')
-ormolu --mode inplace $HS_FILES && echo "Ormolu success!"
