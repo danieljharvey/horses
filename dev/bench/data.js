10f059ea687b85e015d2d57d6047458e7bd95618
github-action-benchmark <github@users.noreply.github.com>
2023-03-05 21:05:32 +0000

    add Store benchmarks in Github Pages (customSmallerIsBetter) benchmark result for 6ecacd89510b32a36e466c198a298e89bbf61832

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678050034021,
+  "lastUpdate": 1678050332294,
   "repoUrl": "https://github.com/danieljharvey/mimsa",
   "entries": {
     "Store benchmarks in Github Pages": [
@@ -39,6 +39,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "evaluate/evaluate parsing 2",
             "value": 0.21307061017143303,
+            "unit": "Time (ms)"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "danieljamesharvey@gmail.com",
+            "name": "Daniel Harvey",
+            "username": "danieljharvey"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6ecacd89510b32a36e466c198a298e89bbf61832",
+          "message": "Fix reporting (#894)\n\n* oh\r\n\r\n* Run always\r\n\r\n* What",
+          "timestamp": "2023-03-05T21:03:29Z",
+          "tree_id": "ce8eb77a919195b21e957304bfdae5f114ba6181",
+          "url": "https://github.com/danieljharvey/mimsa/commit/6ecacd89510b32a36e466c198a298e89bbf61832"
+        },
+        "date": 1678050331406,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "build stdlib/allFns",
+            "value": 0.1065179896833332,
+            "unit": "Time (ms)"
+          },
+          {
+            "name": "evaluate/evaluate big looping thing",
+            "value": 0.09472084221666953,
+            "unit": "Time (ms)"
+          },
+          {
+            "name": "evaluate/evaluate parsing",
+            "value": 0.03252622537254985,
+            "unit": "Time (ms)"
+          },
+          {
+            "name": "evaluate/evaluate parsing 2",
+            "value": 0.2342738394857122,
             "unit": "Time (ms)"
           }
         ]
