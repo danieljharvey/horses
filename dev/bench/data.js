7290ef1c3031ce408e28f9cad13b8881e44c4e46
github-action-benchmark <github@users.noreply.github.com>
2023-03-05 21:00:34 +0000

    add Store benchmarks in Github Pages (customSmallerIsBetter) benchmark result for 363f9a45b98693c5cc31534f961fd2d094a13026

---
+++ dev/bench/data.js
@@ -0,0 +1,48 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1678050034021,
+  "repoUrl": "https://github.com/danieljharvey/mimsa",
+  "entries": {
+    "Store benchmarks in Github Pages": [
+      {
+        "commit": {
+          "author": {
+            "name": "danieljharvey",
+            "username": "danieljharvey"
+          },
+          "committer": {
+            "name": "danieljharvey",
+            "username": "danieljharvey"
+          },
+          "id": "363f9a45b98693c5cc31534f961fd2d094a13026",
+          "message": "Fix reporting",
+          "timestamp": "2023-02-10T05:45:57Z",
+          "url": "https://github.com/danieljharvey/mimsa/pull/894/commits/363f9a45b98693c5cc31534f961fd2d094a13026"
+        },
+        "date": 1678050033073,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "build stdlib/allFns",
+            "value": 0.10609351284999956,
+            "unit": "Time (ms)"
+          },
+          {
+            "name": "evaluate/evaluate big looping thing",
+            "value": 0.09067831138181617,
+            "unit": "Time (ms)"
+          },
+          {
+            "name": "evaluate/evaluate parsing",
+            "value": 0.03263726341666585,
+            "unit": "Time (ms)"
+          },
+          {
+            "name": "evaluate/evaluate parsing 2",
+            "value": 0.21307061017143303,
+            "unit": "Time (ms)"
+          }
+        ]
+      }
+    ]
+  }
+}
