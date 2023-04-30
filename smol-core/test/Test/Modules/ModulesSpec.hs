406314f704223ac70e9be909ba50ad57973b9ab2
Daniel Harvey <danieljamesharvey@gmail.com>
2023-04-30 14:24:48 +0100

    Need to include module types in module expression typechecking

--- smol-core/test/Test/Modules/ModulesSpec.hs
+++ smol-core/test/Test/Modules/ModulesSpec.hs
@@ -14,6 +14,7 @@ import Data.Text (Text)
 import qualified Data.Text.Encoding as T
 import Smol.Core
 import Smol.Core.Modules.FromParts
+import Smol.Core.Modules.ModuleError
 import Smol.Core.Modules.ResolveDeps
 import Smol.Core.Modules.Typecheck
 import Smol.Core.Types.Module.DefIdentifier
@@ -26,10 +27,20 @@ testInputs :: [(FilePath, Text)]
 testInputs =
   fmap (second T.decodeUtf8) $(makeRelativeToProject "test/static/" >>= embedDir)
 
-preludeInput :: Text
-preludeInput = case find (\(filename, _) -> "Prelude" `isInfixOf` filename) testInputs of
+getModuleInput :: String -> Text
+getModuleInput moduleName = case find (\(filename, _) -> moduleName `isInfixOf` filename) testInputs of
   Just (_, code) -> code
-  Nothing -> error "could not find prelude code"
+  Nothing -> error $ "could not find " <> moduleName <> " code"
+
+testModuleTypecheck :: String -> Either ModuleError (Module ResolvedDep (Type ResolvedDep Annotation))
+testModuleTypecheck moduleName =
+  case parseModuleAndFormatError (getModuleInput moduleName) of
+    Right moduleParts -> do
+      case moduleFromModuleParts mempty moduleParts >>= resolveModuleDeps of
+        Left e -> error (show e)
+        Right myModule -> do
+          typecheckModule mempty "" myModule
+    Left e -> error (show e)
 
 spec :: Spec
 spec = do
@@ -160,10 +171,6 @@ spec = do
 
     describe "Typecheck" $ do
       it "Typechecks Prelude successfully" $ do
-        case parseModuleAndFormatError preludeInput of
-          Right moduleParts -> do
-            case moduleFromModuleParts mempty moduleParts >>= resolveModuleDeps of
-              Left e -> error (show e)
-              Right myModule -> do
-                typecheckModule mempty "" myModule `shouldSatisfy` isRight
-          Left e -> error (show e)
+        testModuleTypecheck "Prelude" `shouldSatisfy` isRight
+      it "Typechecks Maybe successfully" $ do
+        testModuleTypecheck "Maybe" `shouldSatisfy` isRight
