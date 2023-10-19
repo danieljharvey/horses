597319d8595cc2ad05e9e12d545d127621dffb86
Daniel Harvey <danieljamesharvey@gmail.com>
2023-10-19 15:07:37 +0100

    Format everything

--- smol-core/test/Test/Modules/CheckSpec.hs
+++ smol-core/test/Test/Modules/CheckSpec.hs
@@ -4,22 +4,20 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 -- | test evaluating and running tests for a module
-
 module Test.Modules.CheckSpec (spec) where
-import Smol.Core.Modules.Check
-import Smol.Core.Modules.RunTests
 
-
-import Data.Functor
 import Data.Bifunctor (second)
 import Data.FileEmbed
-import Data.Foldable ( traverse_)
+import Data.Foldable (traverse_)
+import Data.Functor
+import Data.Monoid
 import Data.Text (Text)
 import qualified Data.Text.Encoding as T
 import Smol.Core
+import Smol.Core.Modules.Check
+import Smol.Core.Modules.RunTests
 import Smol.Core.Modules.Types.ModuleError
 import Test.Hspec
-import Data.Monoid
 
 -- these are saved in a file that is included in compilation
 testInputs :: [(FilePath, Text)]
@@ -38,15 +36,14 @@ testCheckModule input =
       checkModule input moduleParts <&> \tcModule ->
         let testResults = runTests tcModule
          in if testsAllPass testResults
-            then ()
-            else error (show testResults)
+              then ()
+              else error (show testResults)
 
 spec :: Spec
 spec = do
   describe "Modules" $ do
     describe "Check" $ do
-      let testModule (filepath,input) =
-                  it ("Checks " <> filepath <> " successfully") $
-                     testCheckModule input `shouldBe` Right ()
+      let testModule (filepath, input) =
+            it ("Checks " <> filepath <> " successfully") $
+              testCheckModule input `shouldBe` Right ()
       traverse_ testModule testInputs
-
