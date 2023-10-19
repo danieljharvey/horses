169279b1a530042167d87f813f9f9361be5385fa
Daniel Harvey <danieljamesharvey@gmail.com>
2023-10-19 15:13:18 +0100

    Run module tests (#1019)
    * Run module tests
    
    * Format everything

--- smol-core/test/Test/Modules/TypecheckSpec.hs
+++ smol-core/test/Test/Modules/TypecheckSpec.hs
@@ -8,16 +8,13 @@ module Test.Modules.TypecheckSpec (spec) where
 import Data.Bifunctor (second)
 import Data.Either (isRight)
 import Data.FileEmbed
-import Data.Foldable (find)
-import Data.List (isInfixOf)
+import Data.Foldable (traverse_)
 import Data.Text (Text)
 import qualified Data.Text.Encoding as T
-import Error.Diagnose (defaultStyle, printDiagnostic, stdout)
 import Smol.Core
 import Smol.Core.Modules.Check
 import Smol.Core.Modules.Types hiding (Entity (..))
 import Smol.Core.Modules.Types.ModuleError
-import System.IO.Unsafe
 import Test.Helpers
 import Test.Hspec
 
@@ -25,11 +22,6 @@ import Test.Hspec
 testInputs :: [(FilePath, Text)]
 testInputs =
   fmap (second T.decodeUtf8) $(makeRelativeToProject "test/static/" >>= embedDir)
-
-getModuleInput :: String -> Text
-getModuleInput moduleName = case find (\(filename, _) -> moduleName `isInfixOf` filename) testInputs of
-  Just (_, code) -> code
-  Nothing -> error $ "could not find " <> moduleName <> " code"
 
 testTypecheck ::
   Text ->
@@ -40,21 +32,6 @@ testTypecheck input =
   case parseModuleAndFormatError input of
     Right moduleParts -> checkModule input moduleParts
     Left e -> error (show e)
-
-testModuleTypecheck ::
-  String ->
-  Either
-    (ModuleError Annotation)
-    (Module ResolvedDep (Type ResolvedDep Annotation))
-testModuleTypecheck moduleName =
-  case testTypecheck (getModuleInput moduleName) of
-    Left e -> Left (showModuleError e)
-    Right a -> pure a
-
-showModuleError :: ModuleError Annotation -> ModuleError Annotation
-showModuleError modErr = unsafePerformIO $ do
-  printDiagnostic stdout True True 2 defaultStyle (moduleErrorDiagnostic modErr)
-  pure modErr
 
 spec :: Spec
 spec = do
@@ -88,35 +65,7 @@ spec = do
             _ -> False
 
     describe "Typecheck" $ do
-      it "Typechecks Prelude successfully" $ do
-        testModuleTypecheck "Prelude" `shouldSatisfy` isRight
-
-      it "Typechecks Maybe successfully" $ do
-        testModuleTypecheck "Maybe" `shouldSatisfy` isRight
-
-      it "Typechecks Either successfully" $ do
-        testModuleTypecheck "Either" `shouldSatisfy` isRight
-
-      it "Typechecks Expr successfully" $ do
-        testModuleTypecheck "Expr" `shouldSatisfy` isRight
-
-      it "Typechecks Reader successfully" $ do
-        testModuleTypecheck "Reader" `shouldSatisfy` isRight
-
-      it "Typechecks State successfully" $ do
-        testModuleTypecheck "State" `shouldSatisfy` isRight
-
-      it "Typechecks Eq successfully" $ do
-        testModuleTypecheck "Eq" `shouldSatisfy` isRight
-
-      it "Typechecks Semigroup successfully" $ do
-        testModuleTypecheck "Semigroup" `shouldSatisfy` isRight
-
-      it "Typecheck Monoid successfully" $ do
-        testModuleTypecheck "Monoid" `shouldSatisfy` isRight
-
-      it "Typecheck Functor successfully" $ do
-        testModuleTypecheck "Functor" `shouldSatisfy` isRight
-
-      it "Typecheck Show successfully" $ do
-        testModuleTypecheck "Show" `shouldSatisfy` isRight
+      let typecheckAModule (filepath, input) =
+            it ("Typechecks " <> filepath <> " successfully") $
+              testTypecheck input `shouldSatisfy` isRight
+      traverse_ typecheckAModule testInputs
