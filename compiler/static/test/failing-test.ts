69fec9d0598b773296f38dcc56a58f5c8fedcc47
Daniel Harvey <danieljamesharvey@gmail.com>
2023-10-25 22:23:34 +0100

    Remove mimsa

--- compiler/static/test/failing-test.ts
+++
@@ -1,8 +0,0 @@
-// this file should not typecheck
-type Horse = {
-  horseName: string
-}
-
-const a: Horse = { horseName: 100 }
-
-console.log('i am a test')
--- core/src/Language/Mimsa/Core/Types/Type.hs
+++
@@ -1,6 +0,0 @@
-module Language.Mimsa.Core.Types.Type
-  ( module Language.Mimsa.Core.Types.Type.MonoType,
-  )
-where
-
-import Language.Mimsa.Core.Types.Type.MonoType
