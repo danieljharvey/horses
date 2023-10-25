b124c34bcb763ae4052ed4c45026ecca0e9e9596
Daniel Harvey <danieljamesharvey@gmail.com>
2023-10-25 08:08:07 +0100

    Bump

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         # current compiler version, ideally, we'll put everything here
         # eventually
 
-        compilerVersion = "ghc962";
+        compilerVersion = "ghc963";
 
         # fix things
         haskell = pkgs.haskell // {
