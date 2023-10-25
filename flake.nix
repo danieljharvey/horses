1db3f11cdcd40bdd719189c2c00e14fa7ccb44d1
Daniel Harvey <danieljamesharvey@gmail.com>
2023-10-25 22:58:10 +0100

    Fix flake nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Mimsa";
+  description = "Horses";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
@@ -10,30 +10,6 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        oldCompilerVersion = "ghc945";
-
-        # we are going to build ormolu and hlint with an old ghc that isn't
-        # broken
-        oldHaskell = pkgs.haskell // {
-          packages = pkgs.haskell.packages // {
-            "${oldCompilerVersion}" =
-              pkgs.haskell.packages."${oldCompilerVersion}".override {
-                overrides = self: super: {
-                  # On aarch64-darwin, this creates a cycle for some reason; didn't look too much into it.
-                  ghcid = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.overrideCabal super.ghcid (drv: { enableSeparateBinOutput = false; }));
-                  # has wrong version of unix-compat, so we ignore it
-                  shelly = pkgs.haskell.lib.doJailbreak super.shelly;
-                };
-
-              };
-          };
-        };
-
-        oldHaskellPackages = haskell.packages.${oldCompilerVersion};
-
-        # current compiler version, ideally, we'll put everything here
-        # eventually
 
         compilerVersion = "ghc963";
 
@@ -51,6 +27,11 @@
                   cabal-fmt = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.overrideCabal super.cabal-fmt (drv: {
                     enableSeparateBinOutput = false;
                   }));
+                  # try and remove cycle
+                  ormolu = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.overrideCabal super.ormolu (drv: {
+                    enableSeparateBinOutput = false;
+                  }));
+
                 };
 
               };
@@ -62,7 +43,7 @@
         jailbreakUnbreak = pkg:
           pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
 
-        packageName = "mimsa";
+        packageName = "horses";
       in
       {
         # we're not interested in building with Nix, just using it for deps
@@ -72,24 +53,16 @@
 
         devShell = pkgs.mkShell {
           buildInputs = with haskellPackages; [
-            oldHaskellPackages.hlint
-            oldHaskellPackages.ormolu
+            hlint
+            ormolu
             # haskell-language-server # this simply does nothing atm
             ghcid
             cabal-fmt
             cabal-install
             ghc
-            pkgs.zlib # used by `digest` package
-            pkgs.nodejs-18_x
             pkgs.clang_14
             pkgs.llvmPackages_14.llvm
-            pkgs.nodePackages.ts-node
           ];
-
-          # put clang_14 on the path
-          #shellHook = with pkgs; ''
-          # export PATH="${clang_14}/bin:$PATH"
-          #'';
 
           inputsFrom = builtins.attrValues self.packages.${system};
         };
